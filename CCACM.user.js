60d351fce17f9cc7cc1813ae675a1a4de537c3be
ぼぶ <tybob8010@gmail.com>
2026-03-15 16:47:26 +0900

    一部追記
    主に読み込み方式をGame.LoadModに変更

--- CCACM.user.js
+++ CCACM.user.js
@@ -1,20 +1,27 @@
 // ==UserScript==
-// @name         CCACM (Cookie Clicker Auto Closing Mod)
-// @namespace    http://tampermonkey.net/
+// @name         CCACM(Cookie Clicker Auto Closing MOD)
+// @namespace    https://tybob8010.github.io/CCACM/
 // @version      1.0.0
 // @description  CookieClickerを自動で終了させるMOD。指定時間にセーブしてタブを閉じます。
 // @author       tybob
 // @match        https://orteil.dashnet.org/cookieclicker/
 // @grant        window.close
 // @run-at       document-idle
+// @downloadURL  https://tybob8010.github.io/CCACM/CCACM.user.js
+// @updateURL    https://tybob8010.github.io/CCACM/CCACM.user.js
 // ==/UserScript==
 
 (function() {
-    // あなたのGitHub PagesのURL、またはRawのURLを指定
-    const scriptUrl = 'https://tybob8010.github.io/CCACM/CCACM.js';
-    
-    const script = document.createElement('script');
-    script.src = scriptUrl;
-    script.id = 'CCACM_script';
-    document.head.appendChild(script);
+    'use strict';
+    // 本体ファイルを公式のLoadModで読み込む
+    if (typeof Game !== 'undefined' && Game.ready) {
+        Game.LoadMod('https://tybob8010.github.io/CCACM/CCACM.js');
+    } else {
+        const checkReady = setInterval(function() {
+            if (typeof Game !== 'undefined' && Game.ready) {
+                Game.LoadMod('https://tybob8010.github.io/CCACM/CCACM.js');
+                clearInterval(checkReady);
+            }
+        }, 1000);
+    }
 })();
