8a1b87ea8776562da61ba829f86e18f322a7c42f
ぼぶ <tybob8010@gmail.com>
2026-03-16 20:46:30 +0900

    v.1.0.1
    タブを閉じる際about:blankにリダイレクトしてタブが閉じないバグを改善（？できなのかな？）

--- CCACM.user.js
+++ CCACM.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         CCACM(Cookie Clicker Auto Closing MOD)
-// @namespace    https://tybob8010.github.io/CCACM/
-// @version      1.0.0
-// @description  CookieClickerを自動で終了させるMOD。指定時間にセーブしてタブを閉じます。
+// @namespace    https://github.com/tybob8010/CCACM
+// @version      1.0.1
+// @description  CookieClickerを自動で終了させるMOD。指定時間になるとセーブしてタブを閉じます。
 // @author       tybob
 // @match        https://orteil.dashnet.org/cookieclicker/
 // @grant        window.close
+// @grant        GM_closeWindow
 // @run-at       document-idle
 // @downloadURL  https://tybob8010.github.io/CCACM/CCACM.user.js
 // @updateURL    https://tybob8010.github.io/CCACM/CCACM.user.js
@@ -13,7 +14,17 @@
 
 (function() {
     'use strict';
-    // 本体ファイルを公式のLoadModで読み込む
+
+    // 本家サイトの制限を回避するための終了関数を定義
+    window.closeCCACM = function() {
+        if (typeof GM_closeWindow !== "undefined") {
+            GM_closeWindow(); // Tampermonkeyの特権で閉じる
+        } else {
+            window.open('', '_self');
+            window.close();
+        }
+    };
+
     if (typeof Game !== 'undefined' && Game.ready) {
         Game.LoadMod('https://tybob8010.github.io/CCACM/CCACM.js');
     } else {
