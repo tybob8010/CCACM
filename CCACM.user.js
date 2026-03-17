74ae6d61afe10bf64da2465fa754b868f9c949b6
ぼぶ <tybob8010@gmail.com>
2026-03-17 14:10:03 +0900

    v.1.0.2

--- CCACM.user.js
+++ CCACM.user.js
@@ -1,29 +1,19 @@
 // ==UserScript==
 // @name         CCACM(Cookie Clicker Auto Closing MOD)
-// @namespace    https://github.com/tybob8010/CCACM
-// @version      1.0.1
-// @description  CookieClickerを自動で終了させるMOD。指定時間になるとセーブしてタブを閉じます。
+// @namespace    https://tybob8010.github.io/CCACM/
+// @version      1.0.2
+// @description  CookieClickerを自動で終了させるMOD。指定時間にセーブしてタブを閉じます。
 // @author       tybob
 // @match        https://orteil.dashnet.org/cookieclicker/
 // @grant        window.close
-// @grant        GM_closeWindow
-// @run-at       document-idle
+// @run-at       document-start
+// @require      https://tybob8010.github.io/CCACM/CCACM.js
 // @downloadURL  https://tybob8010.github.io/CCACM/CCACM.user.js
 // @updateURL    https://tybob8010.github.io/CCACM/CCACM.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    // 本家サイトの制限を回避するための終了関数を定義
-    window.closeCCACM = function() {
-        if (typeof GM_closeWindow !== "undefined") {
-            GM_closeWindow(); // Tampermonkeyの特権で閉じる
-        } else {
-            window.open('', '_self');
-            window.close();
-        }
-    };
 
     if (typeof Game !== 'undefined' && Game.ready) {
         Game.LoadMod('https://tybob8010.github.io/CCACM/CCACM.js');
