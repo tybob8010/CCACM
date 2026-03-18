699404fb5510633183c14daf208f1c8909bed230
ぼぶ <tybob8010@gmail.com>
2026-03-18 17:58:07 +0900

    v.1.0.3_pre2

--- CCACM.user.js
+++ CCACM.user.js
@@ -1,14 +1,12 @@
 // ==UserScript==
-// @name         CCACM(Cookie Clicker Auto Closing MOD)
-// @namespace    https://tybob8010.github.io/CCACM/
-// @version      1.0.2
+// @name         CCACM (Cookie Clicker Auto Closing MOD)
+// @namespace    https://github.com/tybob8010/CCACM/
+// @version      1.0.3
 // @description  CookieClickerを自動で終了させるMOD。指定時間にセーブしてタブを閉じます。
 // @author       tybob
 // @match        https://orteil.dashnet.org/cookieclicker/
-// @match        https://tybob8010.github.io/cookieclicker/
 // @grant        window.close
-// @grant        GM_closeWindow
-// @run-at       document-start
+// @run-at       document-idle
 // @require      https://tybob8010.github.io/CCACM/CCACM.js
 // @downloadURL  https://tybob8010.github.io/CCACM/CCACM.user.js
 // @updateURL    https://tybob8010.github.io/CCACM/CCACM.user.js
@@ -17,20 +15,18 @@
 (function() {
     'use strict';
 
-    // 本家サイトの制限を回避するための終了関数を定義
-    // これを書いておけば、CCACM.js（本体）側から window.closeCCACM() で呼べます
+    // 【最重要】ブラウザの制限を突破するための「特権関数」をグローバルに公開
+    // これにより、外部ファイルの CCACM.js からこの関数を呼ぶことでタブを閉じることが可能になります。
     window.closeCCACM = function() {
-        if (typeof GM_closeWindow !== "undefined") {
-            GM_closeWindow(); // Tampermonkeyの特権で閉じる
-        } else {
-            window.open('', '_self');
-            window.close();
-        }
+        console.log("CCACM: Closing tab via Tampermonkey privilege...");
+        window.close();
     };
 
-    // 【重要】@require で読み込んでいる場合、すでに CCACM.js の中身は実行されています。
-    // なので、ここで Game.LoadMod を重ねて書く必要はありません。
-    // CCACM.js 側の「registerLoop」が自動的にゲームへの登録を行ってくれます。
-    
-    console.log("CCACM: UserScript wrapper loaded.");
+    // Gameのロード完了を監視してログを出す（デバッグ用）
+    const checkReady = setInterval(function() {
+        if (typeof Game !== 'undefined' && Game.ready) {
+            console.log("CCACM: Game is ready. Mod should be initialized via @require.");
+            clearInterval(checkReady);
+        }
+    }, 1000);
 })();
