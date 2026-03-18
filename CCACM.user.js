a7b1a32fef936b069a8e7c217ec8b663633dbc2c
ぼぶ <tybob8010@gmail.com>
2026-03-18 16:32:31 +0900

    v.1.0.3_pre
    ・ブックマークレットのとき、about:blankになりタブが閉じない
    ・tampermonkeyを使うとき、ユーザースクリプトを許可するをONにしても、起動しない。（ブックマークレットを一回クリックすれば、次回以降はuserscriptで起動する）

--- CCACM.user.js
+++ CCACM.user.js
@@ -5,7 +5,9 @@
 // @description  CookieClickerを自動で終了させるMOD。指定時間にセーブしてタブを閉じます。
 // @author       tybob
 // @match        https://orteil.dashnet.org/cookieclicker/
+// @match        https://tybob8010.github.io/cookieclicker/
 // @grant        window.close
+// @grant        GM_closeWindow
 // @run-at       document-start
 // @require      https://tybob8010.github.io/CCACM/CCACM.js
 // @downloadURL  https://tybob8010.github.io/CCACM/CCACM.user.js
@@ -15,14 +17,20 @@
 (function() {
     'use strict';
 
-    if (typeof Game !== 'undefined' && Game.ready) {
-        Game.LoadMod('https://tybob8010.github.io/CCACM/CCACM.js');
-    } else {
-        const checkReady = setInterval(function() {
-            if (typeof Game !== 'undefined' && Game.ready) {
-                Game.LoadMod('https://tybob8010.github.io/CCACM/CCACM.js');
-                clearInterval(checkReady);
-            }
-        }, 1000);
-    }
+    // 本家サイトの制限を回避するための終了関数を定義
+    // これを書いておけば、CCACM.js（本体）側から window.closeCCACM() で呼べます
+    window.closeCCACM = function() {
+        if (typeof GM_closeWindow !== "undefined") {
+            GM_closeWindow(); // Tampermonkeyの特権で閉じる
+        } else {
+            window.open('', '_self');
+            window.close();
+        }
+    };
+
+    // 【重要】@require で読み込んでいる場合、すでに CCACM.js の中身は実行されています。
+    // なので、ここで Game.LoadMod を重ねて書く必要はありません。
+    // CCACM.js 側の「registerLoop」が自動的にゲームへの登録を行ってくれます。
+    
+    console.log("CCACM: UserScript wrapper loaded.");
 })();
